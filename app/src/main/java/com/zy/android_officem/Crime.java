4051bc0865ba81279d19edf70c8c0309f96c4518
LucaHome <yangzoucn@163.com>
2017-07-16 15:32:34 +0800

    add fragment

--- app/src/main/java/com/zy/android_officem/Crime.java
+++ app/src/main/java/com/zy/android_officem/Crime.java
@@ -1,12 +1,15 @@
 package com.zy.android_officem;
 
 
+import java.util.Date;
 import java.util.UUID;
 
 public class Crime {
 
     private UUID mId;
     private String mTitle;
+    private Date mDate;
+    private boolean mSolved;
 
     public UUID getId() {
         return mId;
@@ -20,10 +23,29 @@ public class Crime {
         mTitle = title;
     }
 
-    public Crime() {
+    public Date getDate() {
+        return mDate;
+    }
 
+    public void setDate(Date date) {
+        mDate = date;
+    }
+
+    public boolean isSolved() {
+        return mSolved;
+    }
+
+    public void setSolved(boolean solved) {
+        mSolved = solved;
+    }
+
+    public Crime() {
 
         // Generate unique identifier
         mId = UUID.randomUUID();
+
+        mDate = new Date();
+
+
     }
 }
